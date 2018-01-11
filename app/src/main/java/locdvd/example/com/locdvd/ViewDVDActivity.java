5e5406fe01931cdfb4344773673f4f1b4e8e0b72
souadef <samir13_mrs@hotmail.fr>
2018-01-11 16:51:46 +0100

    third commit

--- app/src/main/java/locdvd/example/com/locdvd/ViewDVDActivity.java
+++ app/src/main/java/locdvd/example/com/locdvd/ViewDVDActivity.java
@@ -1,10 +1,16 @@
 package locdvd.example.com.locdvd;
 
 import android.app.Activity;
+import android.os.Bundle;
+
 
 /**
  * Created by souadef on 10/01/2018.
  */
 
 public class ViewDVDActivity extends Activity {
+    @Override
+    protected void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
